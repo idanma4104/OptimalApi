2e07f8ec62d0585c1e3deb2f759723ad325180f3
Idan Maman <idanm@upstart42.com>
2020-11-15 10:35:51 +0200

    init

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -7,7 +7,7 @@ namespace  OptimalApi.Controllers
     [Controller]
     public abstract class BaseController : ControllerBase
     {
-        // returns the current authenticated account (null if not logged in)
+        // 1returns the current authenticated account (null if not logged in)
         public User User => (User)HttpContext.Items["User"];
     }
 }
